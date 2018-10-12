8c0cd1c71b57fd73adaf68fc1fdd3ec3bcb75939
David Ham <david.ham@imperial.ac.uk>
2018-10-12 13:50:36 +0100

    Remove --disable-ssh

--- install_firedrake_archer.sh
+++ install_firedrake_archer.sh
@@ -34,6 +34,6 @@ export NETCDF4_DIR=${PWD}/firedrake/lib/python3.7/site-packages/petsc
 echo "Fetching install script"
 curl -O https://raw.githubusercontent.com/firedrakeproject/firedrake/master/scripts/firedrake-install
 echo "Installing"
-python3 firedrake-install --disable-ssh --verbose --no-package-manager --show-petsc-configure-options --mpicc=cc --mpicxx=CC --mpif90=ftn $@
+python3 firedrake-install --verbose --no-package-manager --show-petsc-configure-options --mpicc=cc --mpicxx=CC --mpif90=ftn $@
 
 echo "Done"
